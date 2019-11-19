6c261c79099b2eb970227dab643d48bdc4ef0dba
Mariabt97 <56029889+Mariabt97@users.noreply.github.com>
2019-11-19 16:50:54 +0100

    Aktivering af button
    Forsøg på at aktivere buttons - muligvis er der importeret mere end vi har brug for. Imports stjålet fra eks.

--- src/main/java/dk/aau/address/view/MenuController.java
+++ src/main/java/dk/aau/address/view/MenuController.java
@@ -1,2 +1,37 @@
 package dk.aau.address.view;
 
+/*Importere javafx filer*/
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
+/*Danner klasse for MenuController, der indeholder activering af knappen*/
+
+public class MenuController{
+@FXML
+private Button anatomyButton;
+
+@FXML
+private Button treatmentButton;
+
+@FXML
+private Button faqButton;
+
+@FXML
+	private void initialize() {
+		// Handle Button event.
+		anatomyButton.setOnAction((event) -> {
+			outputTextArea.appendText("Button Action\n");
+		});
+
+}
+}
